283948cf420840a85e89a9f86643d0b22492e8fa
Manikandan R <manikandan@digitalchakra.in>
2013-02-11 19:23:58 +0530

    common js

---
+++ assets/js/footer.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function()
+{
+		tweets();
+		srs_list();
+			
+});
+
+function tweets()
+{
+	var html='<p><strong>Tweets :</strong>';
+	$.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=bccicom&count=9&callback=?", 
+  function (data)
+	{
+		for(i=0;i<data.length;i++)
+			{
+				html+='<p><span class = "tweet-user">'+data[i]['user']['screen_name']+' : </span>'+data[i]['text']+'</p>';
+			}
+			$('#tweets').html(html);
+    //console.log(r);
+	});
+}
+
+function srs_list()
+{
+	$.ajax({
+        url: 'schedule/srs_list',
+        type: "GET",
+        dataType:'JSON',
+        success: function(data) {
+			html='<h2>Upcoming Series</h2>';
+			for(i=0;i<data.resultset.length;i++)
+			{
+				html+='<li><a href="schedule/index/'+data.resultset[i]['srs_id']+'">'+data.resultset[i]['srs_name']+'</a></li>';
+			}
+			$('#srs_list').html(html);
+        },
+        error:function()
+        {
+			
+		}    
+    });
+	
+}
