e058a98b4122d6898890843ed3aa18a82781d74d
Manikandan R <manikandan@digitalchakra.in>
2013-02-14 10:43:33 +0530

    limt and ordr by shedule

--- application/models/match.php
+++ application/models/match.php
@@ -14,11 +14,11 @@ class Match extends CI_Model
 		{
 			$today_start =  strtotime(date('d-M-Y',time()).' 00:01');
 			$today_end =  strtotime(date('d-M-Y',time()).' 23:59');
-			$qry1=$this->db->where('utc_time <', $today_start)->get('tbl_matches');
+			$qry1=$this->db->limit($limit, $start)->where('utc_time <', $today_start)->order_by('utc_time', 'desc')->get('tbl_matches');
 			//$this->db->flush_cache();
-			$qry2=$this->db->where('utc_time >', $today_start)->where('utc_time <', $today_end)->get('tbl_matches');
+			$qry2=$this->db->where('utc_time >', $today_start)->where('utc_time <', $today_end)->order_by('utc_time', 'asc')->get('tbl_matches');
 			//$this->db->flush_cache();
-			$qry3=$this->db->limit($limit, $start)->where('utc_time >', $today_end)->get('tbl_matches');
+			$qry3=$this->db->limit($limit, $start)->where('utc_time >', $today_end)->order_by('utc_time', 'asc')->get('tbl_matches');
 			if($qry1->num_rows()>0)
 			{
 				$data=array();
