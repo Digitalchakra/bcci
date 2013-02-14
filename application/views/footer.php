e279c748691420a8eecf652f0d28d9dbc2520817
Manikandan R <manikandan@digitalchakra.in>
2013-02-14 12:10:24 +0530

    minified bootstrap

--- application/views/footer.php
+++ application/views/footer.php
@@ -128,25 +128,9 @@
     <!-- Placed at the end of the document so the pages load faster -->
 
     <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-    <script src="<?php echo base_url('assets/js/google-code-prettify/prettify.js');?>"></script>
-	<script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
-	<script src="<?php echo base_url('assets/js/bootstrap-transition.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-alert.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-modal.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-dropdown.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-scrollspy.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-tab.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-tooltip.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-popover.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-button.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-collapse.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-carousel.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-typeahead.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-affix.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/application.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.lightbox-0.5.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/bootstrap-extra.min.js');?>"></script>
     <script src="<?php echo base_url('assets/js/footer.js'); ?>"></script>
-    <link rel="stylesheet" type="text/css" href="src="<?php echo base_url('assets/css/jquery.lightbox-0.5.css'); ?>" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/jquery.lightbox-0.5.css'); ?>" media="screen" />
     <script type="text/javascript">
     $(function() {
         $('a.lightbox').lightBox();
