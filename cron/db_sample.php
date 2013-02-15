ae839bf76dbd7ab0c582e687d074ec917cfea850
Manikandan R <manikandan@digitalchakra.in>
2013-02-15 14:19:46 +0530

    removed pure mysql connection

--- cron/db_sample.php
+++ cron/db_sample.php
@@ -7,16 +7,5 @@
   $cfg->set_connections(array('development' =>
     'mysql://root:password@localhost/bcci'));
  });
-if(!$con=mysql_connect('localhost','root','password'))
-{
-	$error_msg= "Mysql Connection failed in cron job @ BCCI";
-	error_log($error_msg, 1,"manimani1014@gmail.com");
-	exit;
-}
-if(!$db=mysql_select_db('bcci'))
-{
-	$error_msg= "DB Connection failed in cron job @ BCCI";
-	error_log($error_msg, 1,"manimani1014@gmail.com");
-	exit;
-}
+
 ?>
