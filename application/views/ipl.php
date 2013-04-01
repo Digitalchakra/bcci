1f4249a2fc408e3209a8e8d11fc00cb84fae90d3
Dhatraditya J <aditya@digitalchakra.in>
2013-04-01 20:41:58 +0530

    Added IPL splash page

--- application/views/ipl.php
+++ application/views/ipl.php
@@ -1,89 +1,553 @@
-
-			<div class="container">
-				      <!--Page Heading-->
-					  <div class = "row">
-						<div class = "span12">
-						  <div class = "page-header">
-							  <h3>Team Ranking</h3>
-						  </div>
+<link href="<?php echo base_url('assets/css/bcci-ipl.css'); ?>" rel="stylesheet" type="text/css">
+<header>
+	<div class="container ipl-header" >
+		<div class="row">
+			<div class="span3 ipl-logo">
+				<!--<div class="ipl-logo-inner1">
+					<a href="http://bcci.com/"><div class="ipl-logo1"></div></a>
+				</div>-->
+				<div class="ipl-logo-inner2">
+					<a href="http://bcci.com/"><div class="ipl-logo2"></div></a>
+				</div>
+				
+			</div>
+			<div class="span6 ipl-scoreboard" id="liveScore">
+				<div >
+					<h4 class="ipl-matchState" id="ipl_matchstate"></h4>
+				</div>
+				<div class="row-fluid ipl-sb-team">
+					
+						<div class="row-fluid"> 
+						<div class="span5">
+							<div class="span6 ipl-sb-flag">
+								<div >
+									<h3 id="ipl_team1" class="centerFix">CSK</h3>
+								</div>
+								 
+								<div id="ipl_team1_image" class="ipl-CSK clearBoth centerFix leftFix">
+								</div>
+							</div>
+							<div class="span6 ipl-sb-score">
+								
+									<h3 id="ipl_team1_score">128/4</h3>
+									<h4 id="ipl_team1_over">15.1 Overs</h4>
+									<div class="ipl-sb-rr">
+									<h5 id="rrr">Req. RR:</h5>	
+									<h5 id="crr">Cur. RR:</h5>	
+									</div>
+							</div>
 						</div>
-					  </div>
-                  <div class="row-fluid rankings">
-                  	<div class="span3">
-                        <ul style="list-style:none">
-							<h4>News</h4>
-							<?php
-							foreach($news as $single_news)
-							{
-								echo "======================";
-								echo "<div> title: ".$single_news->title."</div>";
-								echo "<div> description: ".$single_news->description."</div>";
-								echo "<div> pubdate: ".$single_news->pubdate."</div>";
-								echo "<div> img1: ".$single_news->media1."</div>";
-								echo "<div> img2: ".$single_news->media2."</div>";
-							}
-						?>
-						</ul>
-                        <!-- <div class = "pull-right"><a href="#">View All</a></div> -->
-                    </div>
-                    <div class="span3">
-                        <ul style="list-style:none">
-							<h4>Schedule</h4>
-							<?php
-							foreach($schedule['upcoming'] as $single_schedule)
-							{
-								echo "======================";
-								echo "<div> srs_name: ".$single_schedule->srs_name."</div>";
-								$description= explode(',',$single_schedule->description);
-								$teams=explode('vs',$description[0]);
-								echo "<div> team1: ".$teams[0]."</div>";
-								echo "<div> team2: ".$teams[1]."</div>";
-								echo "<div> description: ".$description[1]."</div>";
-								echo "<div> sdate: ".$single_schedule->sdate."</div>";
-								echo "<div> edate: ".$single_schedule->edate."</div>";
-								echo "<div> time: ".$single_schedule->time."</div>";
-								echo "<div> vnu: ".$single_schedule->mt_vnu."</div>";
-							}
-						?>
-						</ul>
-                    </div>
-                    <div class="span3">
-                        <ul style="list-style:none">
-							<h4>Points</h4>
-							<?php
+						<div class="span7">
+							<div class="span6 ipl-sb-striker ">
+								<div class="ipl-batsman">
+
+								</div>
+								<div class="ipl-sb-inner">
+									<h3 id="ipl_striker">MS DHONI</h3>
+									<h4 id="ipl_striker_run">46(25)</h4>
+									<div CLASS="ipl-sb-striker-SET">
+										<!-- <div class="ipl-sb-striker-inner">
+											<h5>4s</h5>	
+											
+											<h6>5</h6>
+										</div>
+										<div class="ipl-sb-striker-inner">
+											<h5>6s</h5>
+											
+											<h6>2</h6>
+										</div> -->
+										<div class="ipl-sb-striker-inner">
+											 <h5>s/r</h5>
+											 
+											<h6 id="ipl_striker_sr">48.21</h6>
+										</div>
+									</div>
+								</div>
+									
+							</div>
+							<div class="span6 ipl-sb-runner">
+								<div class="ipl-runner">
+
+								</div>
+								<div class="ipl-sb-inner">
+									<h3 id="ipl_nonStriker">MS DHONI</h3>
+									<h4 id="ipl_nonStriker_run">46(25)</h4>
+									<div CLASS="ipl-sb-striker-SET">
+										<!-- <div class="ipl-sb-striker-inner">
+											<h5>4s</h5>	
+											
+											<h6>5</h6>
+										</div>
+										<div class="ipl-sb-striker-inner">
+											<h5>6s</h5>
+											
+											<h6>2</h6>
+										</div> -->
+										<div class="ipl-sb-striker-inner">
+											 <h5>s/r</h5>
+											 
+											<h6 id="ipl_striker_sr">48.21</h6>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						</div>
+					
+				</div>
+				
+				<div class="row-fluid ipl-sb-team2">
+					
+						<div class="span5">
+							<div class="span6 ipl-sb-flag">
+								<p style="margin-top:-25px; color:#f4f4f4; background-color:#282828; width:20px; height:20px; margin-left:44px;">Vs</p>
+								<h3 id="ipl_team2" class="centerFix">MI</h3>
+								<div id="ipl_team2_image" class="ipl-MI clearBoth centerFix leftFix">
+								</div>
+
+							</div>
+							<div class="span6 ipl-sb-score top40">
+									<h3 id="ipl_team2_score">128/4</h3>
+									<h4 id="ipl_team2_over">15.1 Overs</h4>
+							</div>
+						</div>
+						<div class="span7">
+							<div class="span4 ipl-sb-striker">
+									<div class="ipl-bowler">
+
+								</div>
+								<div class="ipl-sb-inner-bowler">
+									<h3 id="ipl_strikerbowler">TENDULKAR</h3>
+									<h4>46(25)</h4>
+									
+								</div>
+							</div>
+							<div class="span8 ipl-sb-bowler">
+									<div CLASS="ipl-sb-bowler-SET" >
+									<div class="ipl-sb-bowler-inner" >
+										<h5>O</h5>	
+										
+										<h6 id="ipl_strikerbowler_overs">2</h6>
+									</div>
+									<div class="ipl-sb-bowler-inner">
+										<h5>M</h5>
+										
+										<h6 id="ipl_strikerbowler_maidens">0</h6>
+									</div>
+									<div class="ipl-sb-bowler-inner">
+										 <h5>R</h5>
+										 
+										<h6 id="ipl_strikerbowler_runs">25</h6>
+									</div>
+									<div class="ipl-sb-bowler-inner">
+										 <h5>W</h5>
+										 
+										<h6 id="ipl_strikerbowler_wickets">1</h6>
+									</div>
+									</div>
+									<div class="clearBoth ipl-bowler-lname">
+										<h5 id="strikers_lname"></h5>		
+									</div>
+							</div>
+							
+						</div>
+					
+			</div>
+			</div>
+			<div class="span3 ipl-header3">
+				<div class="ipl-pts">
+					<table border="1">
+					<thead>
+						<tr>
+						<th>Team</th>
+						<th>Pld</th>
+						<th>NRR</th>
+						<th>Pts</th>
+					</tr>
+					</thead>
+					<tbody>
+				<?php
+				$pointCount=1;
+				foreach($points as $single_point)
+				{ 
+					if($pointCount==1)
+					{
+						$pointCount=0;
+						$pointClass="ipl-table-c1";
+					}
+					else
+					{
+						$pointCount=1;
+						$pointClass="ipl-table-c2";
+					}
+					?>
+					<tr class="<?=$pointClass?>">
+						<td><?php echo $single_point->abbreviation; ?></td>
+						<td><?php echo $single_point->played; ?></td>
+						<td><?php echo $single_point->netRunRate; ?></td>
+						<td><?php echo $single_point->points; ?></td>					
+					</tr>
+				<? }
+							?>
+				</tbody>
+					<!-- <tr class="ipl-table-c2">
+						<td>DD</td>
+						<td>0</td>
+						<td>+0.0000</td>
+						<td>0</td>					
+					</tr>
+					<tr class="ipl-table-c1">
+						<td>KKR</td>
+						<td>0</td>
+						<td>+0.0000</td>
+						<td>0</td>					
+					</tr>
+					<tr class="ipl-table-c2">
+						<td>KXIP</td>
+						<td>0</td>
+						<td>+0.0000</td>
+						<td>0</td>					
+					</tr>
+					<tr class="ipl-table-c1">
+						<td>MI</td>
+						<td>0</td>
+						<td>+0.0000</td>
+						<td>0</td>					
+					</tr>
+					<tr class="ipl-table-c2">
+						<td>PWI</td>
+						<td>0</td>
+						<td>+0.0000</td>
+						<td>0</td>					
+					</tr>
+					<tr class="ipl-table-c1">
+						<td>PCB</td>
+						<td>0</td>
+						<td>+0.0000</td>
+						<td>0</td>					
+					</tr>
+					<tr class="ipl-table-c2">
+						<td>RR</td>
+						<td>0</td>
+						<td>+0.0000</td>
+						<td>0</td>					
+					</tr>
+					<tr class="ipl-table-c1">
+						<td>SH</td>
+						<td>0</td>
+						<td>+0.0000</td>
+						<td>0</td>					
+					</tr> -->
+					</table>
+					<a href="<?=base_url('ipl/points')?>">View full table</a>
+				</div>
+
+				<ul class="ipl-teams">
+					<li style="height:200px; width:20px; float:left; ">
+						<div class="ipl-arrow">
+
+						</div>
+					</li>
+					<?php
 							foreach($points as $single_point)
 							{
-								echo "======================";
-								echo "<div> fullName: ".$single_point->fullName."</div>";
+								echo "<li class='ipl-".$single_point->abbreviation." floatLeft'>";
+								echo "<div class='ipl-teams-badge'>";
+								echo "<p>".$single_point->points."</p>";
+								echo "</div>";
+								echo "</li>";
+								/*echo "<div> fullName: ".$single_point->fullName."</div>";
 								echo "<div> abbreviation: ".$single_point->abbreviation."</div>";
 								echo "<div> primaryColor: ".$single_point->primaryColor."</div>";
 								echo "<div> points: ".$single_point->points."</div>";
-								/*echo "<div> played: ".$single_point->played."</div>";
+								echo "<div> played: ".$single_point->played."</div>";
 								echo "<div> position: ".$single_point->position."</div>";
 								echo "<div> netRunRate: ".$single_point->netRunRate."</div>";
 								echo "<div> won: ".$single_point->won."</div>";
 								echo "<div> lost: ".$single_point->lost."</div>";
 								echo "<div> tied: ".$single_point->tied."</div>";
 								echo "<div> noResult: ".$single_point->noResult."</div>";*/
+								
 							}
 							?>
-						</ul>
-                    </div>
-                    <div class="span3">
-                        <ul style="list-style:none">
-							<h4>Photos</h4>
+					
+					<!-- <li class="ipl-csk">
+						<div class="ipl-teams-badge">
+							<p>1</p>
+						</div>
+					</li>
+					<li class="ipl-kkr">
+						<div class="ipl-teams-badge">
+							<p>2</p>
+						</div>
+					</li>
+					<li class="ipl-rcb">
+						<div class="ipl-teams-badge">
+							<p>3</p>
+						</div>
+					</li>
+					<li class="ipl-mi">
+						<div class="ipl-teams-badge">
+							<p>4</p>
+						</div>
+					</li>
+					<li class="ipl-dd">
+						<div class="ipl-teams-badge">
+							<p>5</p>
+						</div>
+					</li>
+					<li class="ipl-kxip">
+						<div class="ipl-teams-badge">
+							<p>6</p>
+						</div>
+					</li>
+					<li class="ipl-pwi">
+						<div class="ipl-teams-badge">
+							<p>7</p>
+						</div>
+					</li>
+					<li class="ipl-rr">
+						<div class="ipl-teams-badge">
+							<p>8</p>
+						</div>
+					</li>
+					<li class="ipl-sh">
+						<div class="ipl-teams-badge">
+							<p>9</p>
+						</div>
+					</li> -->
+				</ul>
+
+			</div>
+		</div>
+	</div>
+
+</header>	 
+<div class="container">
+	<!--======================================================ADs=======================-->
+<div class="row-fluid">
+	<div class="span8 ipl-ads">
+	</div>
+	<div class="span4 ipl-ads">
+	</div>
+</div>
+	<!--=====================================================Content=======================-->
+	<div class="row-fluid ipl-content">
+		<div class="span7">
+			<div class="span4 ipl-cards3">
+				<!--=============================================================Forums=======================-->
+				<div class="ipl-cards-title ipl-forums">
+					<h3>IPL Forums</h3>
+				</div>
+				<div class="ipl-cards-content ">
+					<div class="ipl-cards3-inner">
+						<div class="bg-red">
+
+						</div>
+					</div>
+					
+					<div class="ipl-forumIntro">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut bibendum nisi. Donec a tristique dolor.</p>
+					</div>
+					
+					
+					<button class="ipl-button2">Join the forums</button>
+				</div>
+				<div class="ipl-forums-footer">
+					<a href="<?=base_url('forum')?>"><div class="ipl-forums-arrow"></div></a>
+				</div>
+			</div>
+			
+			<div class="span4 ipl-cards2">
+				<!--=============================================================Schedule=======================-->
+				<div class="ipl-cards-title ipl-schedule">
+					<h3>IPL Schedule</h3>
+				</div>
+						<?php
+						$scheduleCount=1;
+							foreach($schedule['upcoming'] as $single_schedule)
+							{
+								if($scheduleCount==1)
+									{
+										$scheduleCount=0;
+										$scheduleDisplay="";
+										$scheduleId="ipl-schedule-front";
+									}
+									else
+									{
+										$scheduleDisplay="none";
+										$scheduleCount=1;
+										$scheduleId="ipl-schedule-back";
+									}
+								echo "<div class='ipl-cards-content' id='".$scheduleId."' style='display:".$scheduleDisplay."'>";
+								echo "<div class='bg-orange'></div>";
+								echo "<div class='ipl-cards-content-inner '>";
+								echo "<div class='ipl-schedule-title'>";
+								$description= explode(',',$single_schedule->description);
+								$teams=explode('vs',$description[0]);
+								echo "<h4>".$description[1]."</h4></div>";
+								echo "<div class='ipl-schedule-content'>";
+								echo "<div class='ipl-".trim($teams[0])." floatLeft ipl-schedule-50'></div>";
+								echo "<p>Vs</p>";
+								echo "<div class='ipl-".trim($teams[1])." floatLeft ipl-schedule-50'></div></div>";
+
+								echo "<div class='ipl-schedule-match'>";
+								echo "<p>Kolkata Knight Riders <br/>Vs <br/>Delhi Daredevils</p></div>";
+								echo "<div class='ipl-schedule-venue'>";
+								echo "<p>".$single_schedule->sdate." <br/>".$single_schedule->time."<br/>".$single_schedule->mt_vnu."</p></div></div></div>";
+								}
+						?>
+				<!-- <div class="ipl-cards-content">
+					<div class="bg-orange">
+					</div>
+					<div class=	"ipl-cards-content-inner">
+						<div class="ipl-schedule-title">
+							<h4> Match 1 </h4>
+						</div>
+						<div class="ipl-schedule-content">
+							<div class="ipl-csk"></div>
+								<p>Vs</p>
+							<div class="ipl-mi"></div>
+							
+						</div>
+						<div class="ipl-schedule-match">
+							<p>Kolkata Knight Riders <br/>Vs <br/>Delhi Daredevils</p>
+						</div>
+						<div class="ipl-schedule-venue">
+							<p>02 - APR - 2013 <br/>Chepak Stadium, <br/> Chennai</p>
+						</div>
+						
+					</div>
+				</div> -->
+				<div class="ipl-schedule-footer">
+					<a href="<?=base_url('ipl/schedule')?>">
+					<div class="ipl-schedule-arrow"></div></a>
+				</div>
+			</div>
+			<div class="span4 ipl-cards4">
+					<!--========================================================Photos=======================-->
+				<div class="ipl-cards-title ipl-photos">
+					<h3>IPL Photos</h3>
+				</div>
+				<div class="ipl-cards-content">
+					<div class="ipl-cards3-inner">
+						<div class="bg-pink">
+						</div>
+					</div>
+					<div class="ipl-photo-content">
+						<div class='ipl-photo-title'><h4>Day 3 IPL Highlights</h4></div>
+						<ul>
 							<?php
 							foreach($photos as $single_photos)
 							{
-								echo "======================";
-								echo "<div> icon: ".$single_photos->icon."</div>";
-								echo "<div> src: ".$single_photos->src."</div>";
-								echo "<div> description: ".$single_photos->description."</div>";
-								echo "<div> pubdate: ".$single_photos->pubdate."</div>";
-								echo "<div> link: ".$single_photos->link."</div>";
+
+								echo "<li>";
+								echo "<a href=".$single_photos->link." target='_blank'><img src=".$single_photos->icon."></a>";
+								echo "</li>";
 							}
 							?>
+							
 						</ul>
-                    </div>
-                  </div>
-                </div>
+						<!-- <div class="ipl-photo">
+
+						</div>
+						<div class="ipl-photo">
+
+						</div> -->
+					</div>
+					
+				</div>
+				<div class="ipl-photos-footer">
+					<a href="<?=base_url('photos')?>">
+					<div class="ipl-photos-arrow"></div></a>
+				</div>
+			</div>
+
+		</div>
+
+		
+		<div class="span5">
+			
+			<div class="span6 ipl-cards5">
+				<!--==============================================================Polls=======================-->
+				<div class="ipl-cards-title ipl-polls">
+					<h3>IPL Polls</h3>
+				</div>
+				<div class="ipl-cards-content">
+					<div class="ipl-cards3-inner">
+					<!-- <div class="bg-violet">
+					</div> -->
+				</div>
+						
+					<div class="ipl-polls-content">
+						<p>Do you think Sachin Tendulkar should call it a day from Test cricket after Australia’s 4-0 whitewash?</p>
+						<br/>
+						  <input type="radio" id="r1" name="rr" />
+          				  <label for="r1"><span></span>Yes</label>
+          				 
+           				  <input type="radio" id="r2" name="rr" />
+            			  <label for="r2"><span></span>No</label>
+
+            			  <input type="radio" id="r3" name="rr" />
+            			  <label for="r3"><span></span>Can't Say</label>
+
+            			  <button class="ipl-button">Vote</button>
+							
+					</div>
+				</div>
+				<div class="ipl-polls-footer">
+					<!-- <div class="ipl-polls-arrow"></div> -->
+				</div>
+			</div>
+			<div class="span6 ipl-cards1">
+				<div class="ipl-cards-title ipl-news">
+					<h3>IPL News</h3>
+				</div>
+				<div class="ipl-cards-content ">
+					<div class="bg-blue">
+					</div>
+
+					<ul>
+
+						<?php
+							foreach($news as $single_news)
+							{
+								echo "<li>";
+								echo "<p><a href=".$single_news->link." target='_blank'>".$single_news->title."</a></p>";
+
+								echo "<div class='ipl-news-date'>".$single_news->pubdate."</div>";
+								echo "</li>";
+							}
+						?>
+						<!-- <li><p><a href="">Pepsi IPL Opening Ceremony - BUY TICKETS</a></p></li>
+						<li><p><a href="">Salt Lake Stadium to host IPL Opening Ceremony</a></p></li>
+						<li><p><a href="">The Spin in the tale of Valley</a></p></li> -->
+					</ul>
+				</div>
+				<div class="ipl-news-footer" id="iar1">
+					<a href="<?=base_url('news')?>">
+					<div class="ipl-news-arrow"></div></a>
+				</div>
+			</div>
+		</div>	
+	</div>
+</div>
+<!--<footer>
+	<div class="container">
+		<div class="row" >
+			<div class="span12 ipl-footer">
+				<div class="span8">
+					<p>© Copyright IPL, BCCI 2013. All Rights reserved.</p>
+				</div>
+				<div class="span4">
+					<p>Powered by<a><div class="ipl-footer-logo"></div></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</footer>-->
+
+<script src="<?php echo base_url('assets/js/ipl.js');?>"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.js"></script>               
+ 				            
---
+++ assets/js/ipl.js
@@ -0,0 +1,230 @@
+$(document).ready(function()
+{
+		$.myplaceholder={};
+		$.myplaceholder.mylivescore=[];
+		$.myplaceholder.checkreload=0;
+		$.myplaceholder.checkloaded=0;
+		livescore();
+		$('.ipl-header3').hover(function(){ $(".ipl-teams", this).stop().animate({right:'-300px'},{queue:false,duration:180}); }, 
+			function() { $(".ipl-teams", this).stop().animate({right:'0px'},{queue:false,duration:180}); 
+			});	 
+
+    setInterval(function() {
+         livescore();
+			}, 30000);
+     setInterval(function() {
+         //$('#ipl-schedule-back, #ipl-schedule-front').toggle('drop', {direction: 'right'}, 150)
+        // $('#ipl-schedule-back, #ipl-schedule-front').toggle("blind", {}, 'slow');
+			}, 3000);
+});
+
+
+function livescore()
+{
+	 var site='http://webclient.cricbuzz.com/includes/deccanherald/livecricketscore/filter-match-details.json';
+	 // Fall back URL incase the above URL does not work
+	 //var site='http://webclient.cricbuzz.com/includes/deccanherald/livecricketscore/web-client-match-details.json';  
+    // Take the provided url, and add it to a YQL query. Make sure you encode it!  
+    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + site + '"') + '&format=json';  	
+	 $.getJSON( yql,function(result) {
+		var data=result.query.results.json.json
+	//$.getJSON( 'http://localhost/bcci/live',function(result) {
+	//	var data=result
+			listcount=0;
+			option="";
+			$.myplaceholder.mylivescore=[];
+			$('.live_team').removeClass('active');
+			//alert(data[listcount].matchDataType);
+			$(data).each(function()
+			{
+				if(data[listcount].matchDataType=='Live Data')
+				{
+					option+='<li value="'+listcount+'"';
+					if(listcount ===$.myplaceholder.checkreload)
+					{
+						option+= 'class="live_team dark-block active">';
+						option+= '<div class = "arrow"></div>';
+					}else
+					{
+						option+= 'class="live_team dark-block">';
+						option+= '<div class = "arrow"></div>';
+					}
+					
+					option+= '<div class="padding10"><p class="team_name">'+data[listcount]['team1'].shortName+' vs '+data[listcount]['team2'].shortName;
+					if(data[listcount].state=='inprogress')
+					{
+						option+='&nbsp;&nbsp;<sup><img src="'+baseurl+'assets/Images/live.gif"></sup>';
+					}
+					if(data[listcount].matchdesc !='')
+					{
+					option+='</p><span class="yellow small-text">'+data[listcount].matchdesc+', </span>';
+					}
+					else{
+						option+='</p><span class="yellow small-text">'+data[listcount]['type']+', </span>';
+					}
+					option+='<span class="white small-text">'+data[listcount]['venue-city']+' | '+'</span><span class="yellow small-text">'+data[listcount]['match-day']+'</span></div></li>';
+					$.myplaceholder.mylivescore.push(data[listcount]);
+				}
+				listcount++;
+
+				});
+				$('#livescore_dd').html(option);
+
+			/*some bug has to fix
+			 * when ajax is loading make the current selected tab to update
+			 * instead of refresh the list
+			 */
+			livescoredisplay($.myplaceholder.mylivescore[$.myplaceholder.checkreload]);
+			/*if($.myplaceholder.checkreload==0)
+			{
+				$.myplaceholder.checkreload=1;
+				livescoredisplay($.myplaceholder.mylivescore[0]);
+			}
+			else
+			{
+				//livescoredisplay($.myplaceholder.mylivescore[$('#livescore_dd').value]);
+			}*/
+			$('.live_team').click(function()
+			{
+				$('.live_team').removeClass('active');
+				livescoredisplay($.myplaceholder.mylivescore[this.value]);
+				$(this).addClass('active');
+				$.myplaceholder.checkreload=this.value;
+			});
+        });
+}
+
+function livescoredisplay(data)
+{
+	//alert(new Date(data.startdayandtimeGMT));
+				//series_title
+				//$('#series_title').html(data.matchType+" - "+data.matchdesc);
+				$('#livescore_dd_title').html(data['team1'].shortName+' vs '+data['team2'].shortName+'<span class="caret"></span>');
+				$('#ipl_matchstate').html('');
+				$('#ipl_playerstate, #ipl_bowlerstate, .not_live').show();
+				$('#ipl_series_title').html(data.series);
+				//team name
+				$('#ipl_team1').html(data.battingTeamName);
+				$('#ipl_team2').html(data.bowlingTeamName);
+
+				//team icon
+				//$('#ipl_team1_image').removeAttr('class');
+				//$('#ipl_team2_image').removeAttr('class');
+				//$('#ipl_team1_image').addClass("flag flag_"+data.battingTeamName);
+				//$('#ipl_team2_image').addClass("flag flag_"+data.bowlingTeamName);
+				//$('#team1_image').addClass("flag ipl-"+data.battingTeamName);
+				//$('#team2_image').addClass("flag ipl-"+data.bowlingTeamName);
+
+				//batting team score/over
+				$('#ipl_team1_score').html(data['currentBatTeamScore'].runsAndWicket+'*');
+				$('#ipl_team1_over').html(data['currentBatTeamScore'].overs+" Overs");
+
+				//bowling team score/over
+				//if(data[0]['currentBowlTeamScore'].runsAndWicket !="0/0")
+				//{
+				$('#ipl_team2_score').html(data['currentBowlTeamScore'].runsAndWicket);
+				$('#ipl_team2_over').html(data['currentBowlTeamScore'].overs+" Overs");
+				//}
+
+				//RunRate
+				$('#crr').html("CRR : "+data.crr);
+				if(data.rrr == "")
+				{
+					$('#rrr').html("");
+				}
+				else
+				{
+					$('#rrr').html("RRR : "+data.rrr);
+				}
+
+				//batting team score/over
+				$('#ipl_team1_score').html(data['currentBatTeamScore'].runsAndWicket+'*');
+				$('#ipl_team1_over').html(data['currentBatTeamScore'].overs+" Overs");
+
+				//bowling team score/over
+				//if(data[0]['currentBowlTeamScore'].runsAndWicket !="0/0")
+				//{
+				$('#ipl_team2_score').html(data['currentBowlTeamScore'].runsAndWicket);
+				$('#ipl_team2_over').html(data['currentBowlTeamScore'].overs+" Overs");
+				//}
+				$('#ipl_matchstate').html(data.status);
+				//if(1)
+				if(data.state != 'complete')
+				{
+				//status if not playing
+				if(data.state != 'inprogress')
+				{
+					if(data.state === 'stump')
+					{
+						$('#matchstate').html('Stumps - '+data.status);
+					}
+				}
+				//striker's
+				$('#ipl_striker').html(data['striker'].fullName);
+				$('#ipl_nonStriker').html(data['nonStriker'].fullName);
+
+				//striker's runs
+				$('#ipl_striker_run').html("");
+				if(data['striker'].balls !="")
+				{
+					$('#ipl_striker_run').html(data['striker'].runs+"("+data['striker'].balls+")");
+				}
+				
+				$('#ipl_nonStriker_run').html("");
+				if(data['nonStriker'].balls !="")
+				{
+					$('#ipl_nonStriker_run').html(data['nonStriker'].runs+"("+data['nonStriker'].balls+")");
+				}
+
+				//sr
+				$('#ipl_nonStriker_sr').html("");
+				if(data['nonStriker'].balls != ""){
+					if(data['nonStriker'].balls != 0){
+						$('#ipl_nonStriker_sr').html((data['nonStriker'].runs/data['nonStriker'].balls*100).toFixed(2));
+					}
+					else{
+						$('#ipl_nonStriker_sr').html("0.00");
+					}
+				}
+
+				$('#ipl_striker_sr').html("");
+				if(data['striker'].balls != ""){
+					if(data['striker'].balls != 0){
+						$('#ipl_striker_sr').html((data['striker'].runs/data['striker'].balls*100).toFixed(2));
+					}
+					else{
+						$('#ipl_striker_sr').html("0.00");
+					}
+				}
+
+				//last name
+				$('#strikers_lname').html("");
+				if(data['striker'].fullName != "" && data['strikerbowler'].fullName != "")
+				$('#strikers_lname').html(data['strikerbowler'].fullName.split(' ').slice(-1).join(' ')+" to "+data['striker'].fullName.split(' ').slice(-1).join(' '));
+				//split(' ').slice(-1).join(' ');
+
+
+				//bowler
+				$('#ipl_strikerbowler').html(data['strikerbowler'].fullName);
+
+				$('#ipl_strikerbowler_runs').html(data['strikerbowler'].runs);
+
+				$('#ipl_strikerbowler_overs').html(data['strikerbowler'].overs);
+
+				$('#ipl_strikerbowler_maidens').html(data['strikerbowler'].maidens);
+
+				$('#ipl_strikerbowler_wickets').html(data['strikerbowler'].wickets);
+				}
+				else
+				{
+					$('#ipl_playerstate').hide();
+					$('#ipl_bowlerstate').hide();
+				}
+				if(data.state=='preview')
+				{
+				$('#ipl_playerstate, #ipl_bowlerstate, .not_live').hide();
+				$('#ipl_matchstate').html('Starting '+new Date(data.startdayandtimeGMT));
+				}
+
+
+}
