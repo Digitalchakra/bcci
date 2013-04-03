80ccad47dec6fb7e666be2204b7b653decb15202
ManiKandan R <manikandan.digitalchakra.in>
2013-04-03 21:26:50 +0530

    bar css added in js

--- assets/js/ipl.js
+++ assets/js/ipl.js
@@ -290,6 +290,8 @@ function getpoll()
 				$.each(data.resultset.vote_rate, function(i, item) {
 					barvalue=(item/maxvote)*100;
 					$("#pollans"+i).progressbar({ value: barvalue});
+					$("#pollans"+i+" > div").css({ 'background': '#784a9f' });
+					$("#pollans"+i).css({'border':'1px solid #bbbbbb','background': '#cccccc','border-radius':'0px'});
 				});
 			}
 		},
@@ -321,13 +323,15 @@ function setpoll()
 				$.each(data.resultset.poll_ans, function(i, item) {
 					if(data.resultset.vote_rate[i] > maxvote)
 						maxvote=data.resultset.vote_rate[i];
-						list+='<div>'+item+'</div><div id="pollans'+i+'">'+data.resultset.vote_rate[i]+'</div>';
+						list+='<div>'+item+'</div><div style="height:20px; margin-top:5px; border: 1px solid #c2c2c2;" id="pollans'+i+'"><p style="padding-left: 10px; width: 30px; color:#ffffff; float: left; font-size:12px;">'+data.resultset.vote_rate[i]+'<p></div>';
 				});
 				$('#poll_answer').html(list);
 				$('#ipl-polls-button').remove();
 				$.each(data.resultset.vote_rate, function(i, item) {
 					barvalue=(item/maxvote)*100;
 					$("#pollans"+i).progressbar({ value: barvalue});
+					$("#pollans"+i+" > div").css({ 'background': '#784a9f' });
+					$("#pollans"+i).css({'border':'1px solid #bbbbbb','background': '#cccccc','border-radius':'0px'});
 				});
 			}
 		},
