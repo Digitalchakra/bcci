46e06535ba8c7ac586bfe773af283c3e014da983
Balaji <balaji.sp@digitalchakra.in>
2013-04-30 10:17:49 +0530

    Design changes

---
+++ application/views/test.php
@@ -0,0 +1,47 @@
+<link href="<?php echo base_url('assets/css/test.css'); ?>" rel="stylesheet" type="text/css">
+
+<div class="container">
+
+	<div class="row">
+		<div class="span12 cpn_header">
+			<div class="cpn_logo">
+				<div class="logo"></div>	
+			</div>
+			
+		</div>
+	</div>
+	<div class="row">
+		<div class="span12 cpn_title">
+			<h2>Welcome to Bindaas Cricket café!</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span5 cpn1">
+			<p class="cpn_p1">Our café brings you Cricket with a difference, you don't just get Live Scores with Cricket statistics.</p>
+			
+			<p>You get a world that is a lot bigger than that!</p>
+
+			<iframe style="margin-left:14px;" width="350" height="197" src="http://www.youtube.com/embed/Ju8gxy5eI34?rel=0" frameborder="0" allowfullscreen></iframe>
+		</div>
+		<div class="span3 cpn2">
+			<p class="cpn_p1">Get access to your very own Sports social network in one click!</p>
+			
+			<p>Register with us and get access to a lot more fun on Bindaas Cricket café!</p>
+			<br/>
+			<br/>
+			
+			
+		</div>
+		<div class="span4 cpn3">
+			<div class="cpn3_inner">
+
+				<!-- <div class="cpn_icn">
+				</div> -->
+			</div>
+			<div>
+				<button class="btn btn-warning cpn_join"> Join the café</button>
+			</div>
+			
+		</div>
+	</div>
+</div>
