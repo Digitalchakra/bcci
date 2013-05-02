3d454f41a96c785deb425b2b2c0b1ca4e6765ee9
ManiKandan R <manikandan.digitalchakra.in>
2013-05-02 17:16:04 +0530

    login and register added in camp page

--- application/views/test.php
+++ application/views/test.php
@@ -1,5 +1,5 @@
 <link href="<?php echo base_url('assets/css/test.css'); ?>" rel="stylesheet" type="text/css">
-
+<? $userdata=$this->phpbb_library->get_session_user(); ?>
 <div class="container">
 
 	<div class="row">
@@ -38,9 +38,101 @@
 			
 		</div>
 		<div class="span4 cpn3">
-			<div>
+
+			<!-- if not logged in show registration and login form -->
+			 <?php if($userdata['is_registered'] != 1)
+        { ?>
+        <li class = "dropdown menu_right">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-right:5px">
+              <i class = "icon-lock icon-white" style="margin-right:15px;"></i>Register
+            </a>
+                <div class="dropdown-menu pull-right" role="menu" aria-labelledby="dLabel">
+                  <div class="row">
+                    <div class = "span3">
+                    <div class="padding20" style="text-align:left">
+                      <h4 style = "text-align:left;margin-left:0;">Registration</h4>
+                      <div class="divider"></div>
+                      <span id="cerror_msg1" class="errorMessage"></span>
+
+                      <form class="form-horizontal" id="cregister_form">
+                         <div class="margint10"></div>
+                        <span>Username: </span><input type="text" class="input-large" name="cuname" id="cuname" style="text-align:left">
+                         <div class="margint10"></div>
+                         <span>Password: </span><input type="password" class="input-large" name="cpass_word" id="cinputPassword" style="text-align:left">
+                         <div class="margint10"></div>
+                         <span>E-Mail: </span><input type="text" class="input-large" name="cemail_address" id="cinputEmail" style="text-align:left">
+                         <!--<div class="margint10"></div>
+                         <span>Last Name: </span><input type="text" class="input-large" name="lastname" id="lastname" style="text-align:left">-->
+                         <div class="margint20"></div>
+                         <a class="btn btn-mini btn-primary span1 pull-right marginb15" id="csignupsubmit">Register</a>
+                      </form>
+                      
+                    </div>
+                  </div>
+                  </div>
+                </div>
+          </li>
+          <?php } ?>
+          <?php if($userdata['is_registered'] == 1)
+        { ?>
+        <li class = "menu_right dropdown" >
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-right:5px">
+           <i class = "icon-user icon-white" style="margin-right:15px;"></i><?php echo $userdata['username_clean']; ?>
+          </a>
+          <!-- 
+          <div class="dropdown-menu pull-right" role="menu" aria-labelledby="dLabel">
+           <div class="row">
+              <div class = "span2">
+                <div class="padding20" style="text-align:left">
+-->                <ul class="dropdown-menu pull-right profile-dropdown">
+                   <!-- <li><a tabindex="-1" href="#">Profile</a></li>
+                    <li><a tabindex="-1" href="#">Settings</a></li>-->
+                    <li><a tabindex="-1" href="<?php echo base_url('login/logout'); ?>">Logout</a></li>
+                  </ul>
+<!--                </div>
+              </div>
+            </div>
+         </div>
+         --> 
+        </li>
+        	<div>
+				<button class="btn btn-warning cpn_join"> logged in  :)</button>
+			</div>
+         <?php }
+        else
+        { ?>
+        <li class = "menu_right dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-right:5px">
+           <i class = "icon-user icon-white" style="margin-right:15px;"></i>Login
+          </a>
+          <div class="dropdown-menu pull-right" role="menu" aria-labelledby="dLabel">
+            <div class="row">
+              <div class = "span3">
+                <div class="padding20" style="text-align:left">
+                  
+                  <h4 style = "text-align:left;margin-left:0;" id="clogin_header">Login</h4>
+                  <div class="divider"></div><span id="cerror_msg" class="errorMessage"></span>
+                  <form class="form-horizontal">
+                    <span class="clogin">Username: </span><input type="text" class="input-large clogin" id="cusername" name="cusername" style="text-align:left">
+                    <div class="margint10"></div>
+                    <span class="clogin">Password: </span><input type="password" class="input-large clogin" id="cpassowrd" name="cpassword">
+                    <span id="cforgot_pw_txt"></span><input type="text"  class="cforget input-large margint20" id="cfusername" name="cfusername" style="display:none; margin-top:10px">
+                    <span id="cforget" class="pull-right margint10 clogin red" style="cursor:pointer;margin-left:0;" >Forgot Password?</span>
+                    <div class="margint20"></div>
+                         <a class="btn btn-mini btn-primary span1 pull-right marginb15 margint10 clogin" id="cloginsubmit">Login</a>
+                         <a id="clogin" class="btn btn-mini btn-primary pull-left margint10 cforget red" style="display:none;cursor:pointer;">Cancel</a>
+                         <a class="btn btn-mini btn-primary marginb15 pull-right margint10 cforget" id="cforgetsubmit" style="display:none;">Reset Password</a>
+                  </form>                  
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <div>
 				<button class="btn btn-warning cpn_join"> Join the café</button>
 			</div>
+        <?php } ?>
 			<div class="cpn3_inner">
 
 				<!-- <div class="cpn_icn">
@@ -51,3 +143,4 @@
 		</div>
 	</div>
 </div>
+<script src="<?php echo base_url('assets/js/test.js');?>"></script>
---
+++ assets/js/test.js
@@ -0,0 +1,171 @@
+$(document).ready(function()
+{
+	$('#cforget').click(function(){
+		$('#cfusername').val($('#cusername').val());
+		$('.clogin').hide();
+		$('.cforget').show();
+		$("#clogin_header").text("Forgot Password?");
+		$("#cforgot_pw_txt").text("Username: ");
+	});
+	$('#clogin').click(function(){
+		$('.cforget').hide();
+		$('.clogin').show();
+		$("#clogin_header").text("Login");
+		$("#cforgot_pw_txt").text("");
+	});
+	$('#cfirstname , #clastname , #cinputEmail , #cinputPassword').keypress(function(e) 
+	{
+		if(e.which == 13) 
+		{
+			$('#csignupsubmit').click();
+		}
+	});
+	$('#cusername , #cpassowrd').keypress(function(e) 
+	{
+		if(e.which == 13) 
+		{
+			$('#cloginsubmit').click();
+		}
+	});
+	$('#cloginsubmit').click(function()
+	{
+		$('#cerror_msg').html("");
+		var email = $('#cusername').val();
+		var password = $('#cpassowrd').val();
+		/*var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+		if(!pattern.test(email) || email.length <=0)
+		{
+			$('#apploginusernamecontainer').addClass('control-group error');
+		}
+		else if(password.length <=0)
+		{
+			$('#apploginpasswordcontainer').addClass('control-group error');
+		}*/
+		if(email.length <=0 || password.length <=0)
+		{
+			$('#cerror_msg').html("Enter your email and password.");
+		}
+		else
+		{
+			$.ajax(
+			{
+				url:baseurl+'verifylogin',
+				type:'POST',
+				data:{'username':email,'password':password},
+				dataType: 'json',
+				success:function(data)
+				{
+					if(data.resultset.success=='yes')
+					{
+						//window.location.reload();
+						window.location.href=baseurl+"home";
+					}
+					else
+					{
+						$('#cerror_msg').html(data.resultset.errors);	
+					}
+				},
+				error:function()
+				{
+					$('#cerror_msg').html('Internal error, try agian...');
+				}
+			});
+		}
+	});
+	$('#csignupsubmit').click(function()
+	{
+		$('#cerror_msg1').html("");
+		var email = $('#cinputEmail').val();
+		var password = $('#cinputPassword').val();
+		var uname = $('#cuname').val();
+		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+		
+		if(uname.length <=3)
+		{
+			$('#cerror_msg1').html("Enter valid User name (min 4)");
+		}
+		else if(password.length <=4)
+		{
+			$('#cerror_msg1').html('Enter password.');
+		}
+		else if(!pattern.test(email) || email.length <=0)
+		{
+			//alert('inv');
+			//$('#apploginusernamecontainer').addClass('control-group error');
+			$('#cerror_msg1').html("Enter valid email id.");
+		}
+		else
+		{
+			$.ajax(
+			{
+				url:baseurl+'registration',
+				type:'POST',
+				data:{'uname':uname,'email_address':email,'pass_word':password},
+				dataType: 'json',
+				success:function(data)
+				{
+					if(data.resultset.success=='yes')
+					{
+						$('#cregister_form').hide();
+						$('#cerror_msg1').html("You’re almost done! Please check your inbox to activate your account.");
+					}
+					else
+					{
+						$('#cerror_msg1').html(data.resultset.errors);	
+					}
+				},
+				error:function()
+				{
+					$('#cerror_msg1').html('Internal error, try agian...');
+				}
+			});
+		}
+	});
+	$('#cforgetsubmit').click(function()
+	{
+		$('#cerror_msg').html("");
+		var email = $('#cfusername').val();
+		//var password = $('#passowrd').val();
+		/*var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+		if(!pattern.test(email) || email.length <=0)
+		{
+			$('#apploginusernamecontainer').addClass('control-group error');
+		}
+		else if(password.length <=0)
+		{
+			$('#apploginpasswordcontainer').addClass('control-group error');
+		}*/
+		if(email.length <=0)
+		{
+			$('#cerror_msg').html("Enter your email.");
+		}
+		else
+		{
+			$.ajax(
+			{
+				url:baseurl+'cforget',
+				type:'POST',
+				data:{'username':email},
+				dataType: 'json',
+				success:function(data)
+				{
+					if(data.resultset.success=='yes')
+					{
+						$('#cerror_msg').html("Please check your mail to reset your password");
+						//alert('s');
+						//window.location.reload();
+						//window.location.href="tmplts";
+					}
+					else
+					{
+						$('#cerror_msg').html(data.resultset.errors);	
+					}
+				},
+				error:function()
+				{
+					$('#cerror_msg').html('Internal error, try agian...');
+				}
+			});
+		}
+	});
+});
