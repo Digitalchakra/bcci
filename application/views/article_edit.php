214cad445f59c7686ae06b5ea48d590a8c219ed7
ManiKandan R <manikandan.digitalchakra.in>
2013-05-29 19:19:42 +0530

    style add in article edit and update

--- application/views/article_edit.php
+++ application/views/article_edit.php
@@ -1,4 +1,7 @@
-
+<div class = "container">
+  <div class = "row">
+    <!--Fixtures-->
+    <div class = "span12">
 	<? if($content)
 	{
 		$html = 'this <br>is<br/>some<br />text <br    />!';
@@ -6,17 +9,20 @@
 	?>
 	<form method="post" action="<?=base_url('article/save')?>">
 		<input type="hidden" name="art_id" value="<?=$content[0]->id?>">
-		Author Name: <input type="text" name="author" value="<?=$content[0]->author?>"> <br>
-		Title: <input type="text" name="title" value="<?=$content[0]->title?>"><br>
-		Content: <textarea style="width:600px; height:200px" name="content"> <?=$text?></textarea><br>
-		<input type="submit" value="Update">
+		<p>Author Name</p> <input type="text" name="author" value="<?=$content[0]->author?>"> <br>
+		<p>Title</p> <input type="text" name="title" value="<?=$content[0]->title?>"><br>
+		<p>Content</p><textarea style="width:600px; height:200px" name="content"> <?=$text?></textarea><br>
+		<input type="submit" class="btn btn-success" value="Update">
 	</form>
 	<? } else 
 	{?>
 	<form method="post" action="<?=base_url('article/save')?>">
-		Author Name: <input type="text" name="author" ><br>
-		Title: <input type="text" name="title"><br>
-		Content: <textarea style="width:600px; height:200px" name="content"></textarea><br>
-		<input type="submit" value="Add">
+		<p>Author Name</p> <input type="text" name="author" ><br>
+		<p>Title</p><input type="text" name="title"><br>
+		<p>Content</p><textarea style="width:600px; height:200px" name="content"></textarea><br>
+		<input type="submit" class="btn btn-success" value="Add">
 	</form>
 	<?}?>
+</div>
+</div>
+</div>
