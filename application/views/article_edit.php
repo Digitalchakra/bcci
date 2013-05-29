5f3e04167d6092a70671baef9f6c9b6abb5c8d98
ManiKandan R <manikandan.digitalchakra.in>
2013-05-29 18:51:45 +0530

    artical add / update page align

--- application/views/article_edit.php
+++ application/views/article_edit.php
@@ -6,17 +6,17 @@
 	?>
 	<form method="post" action="<?=base_url('article/save')?>">
 		<input type="hidden" name="art_id" value="<?=$content[0]->id?>">
-		Author Name: <input type="text" name="author" value="<?=$content[0]->author?>">
-		Title: <input type="text" name="title" value="<?=$content[0]->title?>">
-		Content: <textarea name="content"> <?=$text?></textarea>
-		<input type="submit">
+		Author Name: <input type="text" name="author" value="<?=$content[0]->author?>"> <br>
+		Title: <input type="text" name="title" value="<?=$content[0]->title?>"><br>
+		Content: <textarea style="width:600px; height:200px" name="content"> <?=$text?></textarea><br>
+		<input type="submit" value="Update">
 	</form>
 	<? } else 
 	{?>
 	<form method="post" action="<?=base_url('article/save')?>">
-		Author Name: <input type="text" name="author" >
-		Title: <input type="text" name="title">
-		Content: <textarea name="content"></textarea>
-		<input type="submit">
+		Author Name: <input type="text" name="author" ><br>
+		Title: <input type="text" name="title"><br>
+		Content: <textarea style="width:600px; height:200px" name="content"></textarea><br>
+		<input type="submit" value="Add">
 	</form>
 	<?}?>
